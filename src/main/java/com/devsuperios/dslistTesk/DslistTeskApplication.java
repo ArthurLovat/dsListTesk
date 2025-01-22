07e541f16c4f387aaaa431253cc443052168dc9c
ArthurLovat <articfin.eco@gmail.com>
2025-01-21 22:17:05 -0300

    second commit

---
+++ src/main/java/com/devsuperios/dslistTesk/DslistTeskApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperios.dslistTesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslistTeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslistTeskApplication.class, args);
+	}
+
+}
